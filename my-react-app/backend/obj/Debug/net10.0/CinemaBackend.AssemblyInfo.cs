b8f87bfa64c75d2812828aeaece6c2b4bb15b17f
Phạm Văn Huy <huy12456vn@example.com>
2026-09-18 20:17:48 +0700

    Update code

--- my-react-app/backend/obj/Debug/net10.0/CinemaBackend.AssemblyInfo.cs
+++ my-react-app/backend/obj/Debug/net10.0/CinemaBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec81e24b11efdaaf727ab26c0290d16b2d3a8022")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dca21b727823f219e86d17cffa4717bd3694dc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
